3f8a2c58b48df8646a98079ea4fee420cd7160c3
leslie pacheco <lesli_sharai_pacheco@hotmail.com>
2024-07-23 11:23:21 -0600

    Primer commit

---
+++ data.js
@@ -0,0 +1,267 @@
+export default{
+    abount :{
+        "name":"Leslie Pacheco",
+        "title": "Ingeniero en Sistemas Computacionales",
+        "photo": "img/leslieP.jpg",
+        "linkedin":"https://www.linkedin.com/in/lesliesharaipacheco/",
+        "gitHub": "https://github.com/LesliePacheco91",
+        "contact":{
+          "email": "lesli_sharai_pacheco@hotmail.com",
+          "phone": "+(52) 9971135783",
+          "address": "Mérida Yucatán México"
+        },
+        "summary": "Ingeniera en Sistemas Computacionales y Desarrolladora Web con más de 3 años de experiencia en la industria. Mi especialización en Front-end, combinada con mi pasión por el diseño y la programación, me ha permitido crear soluciones tecnológicas innovadoras que optimizan la eficiencia y facilitan el logro de objetivos. El en trabajo en equipo, autoaprendizaje, adaptabilidad y mi creatividad ha sido clave para implementar soluciones efectivas y superar desafíos con éxito. Me rijo por principios de honestidad, responsabilidad y organización, y estoy profundamente comprometida con el avance tecnológico y su impacto positivo en la sociedad.",
+        "experience": [
+          {
+            "company": "Empresa de Desarrollo",
+            "position": "Desarrolladora Web Junior",
+            "startDate": "2022-01",
+            "endDate": "2024-07",
+            "responsibilities": [
+              "Desarrollar y mantener aplicaciones web utilizando HTML, CSS y JavaScript.",
+              "Colaborar con el equipo de diseño para implementar interfaces de usuario responsivas.",
+              "Realizar pruebas y depuración de código para garantizar la calidad del software."
+            ]
+          },
+          {
+            "company": "Startup Tecnológica",
+            "position": "Pasante de Desarrollo Web",
+            "startDate": "2021-06",
+            "endDate": "2021-12",
+            "responsibilities": [
+              "Asistir en el desarrollo de funcionalidades front-end.",
+              "Participar en reuniones de planificación de proyectos.",
+              "Proporcionar soporte técnico y resolver problemas de usuarios."
+            ]
+          }
+        ],
+        "couses":[
+
+            {
+                "id":"1",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            },
+            {
+                "id":"2",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            },
+
+            {
+                "id":"1",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            },
+            {
+                "id":"2",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            },
+
+            {
+                "id":"1",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            },
+            {
+                "id":"2",
+                "name":"name curse",
+                "date":"date",
+                "academy":"udemy",
+                "document":"document",
+                "url": "url",
+            }
+        ],
+        "education": [
+          {
+            "institution": "Universidad Ejemplo",
+            "degree": "Licenciatura en Informática",
+            "startDate": "2018-09",
+            "endDate": "2022-05"
+          }
+        ],
+        "techSkills": [
+
+            {
+                "id":"1",
+                "image":"img/skills/html-5.png",
+                "name":"HTML"
+            },
+            {
+                "id":"2",
+                "image":"img/skills/css-3.png",
+                "name":"CSS"
+            },
+            {
+                "id":"3",
+                "image":"img/skills/js.png",
+                "name":"Javascript"
+            },
+            {
+                "id":"4",
+                "image":"img/skills/bootstrap.png",
+                "name":"HTML"
+            },
+            {
+                "id":"5",
+                "image":"img/skills/php.png",
+                "name":"HTML"
+            },
+            {
+                "id":"6",
+                "image":"img/skills/mysql.png",
+                "name":"HTML"
+            },
+            {
+                "id":"7",
+                "image":"img/skills/laravel.jpg",
+                "name":"HTML"
+            },
+            {
+                "id":"8",
+                "image":"img/skills/figma.png",
+                "name":"HTML"
+            },
+            {
+                "id":"9",
+                "image":"img/skills/git.png",
+                "name":"HTML"
+            },
+            {
+                "id":"10",
+                "image":"img/skills/github.png",
+                "name":"HTML"
+            },
+        ],
+
+        "softSkills":[
+
+            {
+                "id":"1",
+                "image":"img/softskills/001-lluvia-de-ideas.png",
+                "name":"Pensamiento crítico"
+            },
+            {
+                "id":"2",
+                "image":"img/softskills/002-responsabilidad.png",
+                "name":"Responsabilidad"
+            },
+            {
+                "id":"3",
+                "image":"img/softskills/003-adaptacin.png",
+                "name":"Adaptabilidad"
+            },
+            {
+                "id":"4",
+                "image":"img/softskills/004-empata.png",
+                "name":"Empatía"
+            },
+            {
+                "id":"5",
+                "image":"img/softskills/005-honestidad.png",
+                "name":"Honestidad"
+            },
+            {
+                "id":"6",
+                "image":"img/softskills/006-unido.png",
+                "name":"Trabajo en equipo"
+            },
+        ],
+
+        "proyects":[
+            {
+                "id":"1",
+                "name":"Text Analiser",
+                "description":"Aplicación que se encarga de realizar el conteo de palabras, caracteres y numero al ingresar una cadena de texto.",
+                "image":"img/portafolio/Screenshot1.png",
+                "languales":[
+                    {
+                        "id":"1",
+                        "image":"img/skills/html-5.png",
+                        "name":"HTML"
+                    },
+                    {
+                        "id":"2",
+                        "image":"img/skills/css-3.png",
+                        "name":"CSS"
+                    },
+                    {
+                        "id":"3",
+                        "image":"img/skills/js.png",
+                        "name":"Javascript"
+                    },
+                ],
+                "url":"https://lesliepacheco91.github.io/DEV011-text-analyzer/",
+                "repository":"https://github.com/LesliePacheco91/DEV011-text-analyzer/tree/main",
+            }, {
+                "id":"2",
+                "name":"Sistema de evaluación docente",
+                "description":"Aplicación web que tiene la funcionalidad de generar los resultados obtenidos de la evaluación docente realizados por los alumnos mediante un formulario web.",
+                "image":"img/portafolio/2.png",
+                "languales":[
+                    {
+                        "id":"1",
+                        "image":"img/skills/html-5.png",
+                        "name":"HTML"
+                    },
+                    {
+                        "id":"2",
+                        "image":"img/skills/css-3.png",
+                        "name":"CSS"
+                    },
+                    {
+                        "id":"3",
+                        "image":"img/skills/js.png",
+                        "name":"Javascript"
+                    },
+                    {
+                        "id":"4",
+                        "image":"img/skills/bootstrap.png",
+                        "name":"HTML"
+                    },
+                    {
+                        "id":"5",
+                        "image":"img/skills/php.png",
+                        "name":"HTML"
+                    },
+                    {
+                        "id":"6",
+                        "image":"img/skills/mysql.png",
+                        "name":"HTML"
+                    },
+                ],
+                "url":"https://lesliepacheco91.github.io/DEV011-text-analyzer/",
+                "repository":"https://github.com/LesliePacheco91/DEV011-text-analyzer/tree/main",
+            }
+
+        ],
+        "languages": [
+          {
+            "language": "Español",
+            "level": "Nativo"
+          },
+          {
+            "language": "Inglés",
+            "level": "Intermedio"
+          }
+        ]
+      }
+      
+    
+}
---
+++ index.js
@@ -0,0 +1,344 @@
+
+import data from './data.js';
+
+const arrayData = data.abount;
+
+// header seccion
+let container = document.getElementById('container');
+
+let header = document.createElement('header');
+header.setAttribute("class","headerInfo");
+
+let titlPage = document.createElement('h3');
+titlPage.setAttribute("class","titlePage")
+titlPage.textContent ="Portafolio";
+
+let contendMenu = document.createElement("div");
+contendMenu.setAttribute("class","menu");
+
+let list = document.createElement('ul');
+list.className = "listMenu";
+
+let lis1 = document.createElement('li');
+lis1.textContent = "Sobre mí";
+
+let lis2 = document.createElement('li');
+lis2.textContent = "Habilidades";
+let lis3 = document.createElement('li');
+lis3.textContent = "Proyectos";
+let lis4 = document.createElement('li');
+lis4.textContent = "Cursos";
+let lis5 = document.createElement('li');
+lis5.textContent = "Experiencia";
+
+
+list.append(lis1,lis2, lis3, lis4, lis5);
+
+contendMenu.append(list);
+
+header.append(titlPage, contendMenu);
+
+
+/// about me section
+
+let sectionAboutme = document.createElement('section');
+sectionAboutme.setAttribute("id","aboutme");
+
+let photoinfo = document.createElement('div');
+photoinfo.setAttribute("class","photoinfo");
+
+let photo = document.createElement('img');
+photo.setAttribute("class","photo");
+photo.src = arrayData.photo;
+
+let nameInfo = document.createElement('h2');
+nameInfo.setAttribute("class","name");
+nameInfo.textContent = arrayData.name;
+
+let titleinfo = document.createElement('h4');
+titleinfo.setAttribute("class","title");
+titleinfo.textContent = arrayData.title;
+
+let socilaRed = document.createElement('div');
+socilaRed.className = "redes";
+
+// linkedin
+let linkedin = document.createElement('a');
+linkedin.href = arrayData.linkedin;
+
+let imgLinkedin = document.createElement('img');
+imgLinkedin.setAttribute("class","imgRed");
+imgLinkedin.src = 'img/linkedin.png';
+
+linkedin.append(imgLinkedin);
+
+// gitHub
+let git = document.createElement('a');
+git.href = arrayData.gitHub;
+
+let imgGit = document.createElement('img');
+imgGit.setAttribute("class","imgRed");
+imgGit.src = 'img/githubPurple.png';
+
+git.append(imgGit);
+
+socilaRed.append(linkedin, git);
+
+
+let aboutme = document.createElement('div');
+aboutme.setAttribute("class","textAbotuME");
+
+let titleAboutMe = document.createElement('h2');
+titleAboutMe.textContent = "Sobre mí";
+
+let textAbotuMe = document.createElement('p');
+textAbotuMe.textContent = arrayData.summary;
+
+let buttonCv = document.createElement("a");
+buttonCv.setAttribute("class","buttonCV");
+buttonCv.href = "img/cv.pdf";
+buttonCv.textContent = "Descargar CV";
+buttonCv.download = 'cv.pdf';
+
+
+let contact = document.createElement("div");
+contact.setAttribute("class","contact");
+
+
+
+aboutme.append(titleAboutMe, textAbotuMe,buttonCv );
+photoinfo.append(photo, nameInfo,titleinfo, socilaRed);
+sectionAboutme.append(photoinfo, aboutme);
+
+
+// skills section
+let skillsSection = document.createElement('section');
+skillsSection.setAttribute("id","skills");
+
+let titleSkills = document.createElement('h1');
+titleSkills.textContent = "Habilidades";
+
+let titleSkillsTech = document.createElement('h3');
+titleSkillsTech.textContent = "Habilidades técnicas";
+
+let listSkillsTech = document.createElement('ul');
+listSkillsTech.className = "list-kills-tech";
+
+arrayData.techSkills.forEach(item => {
+
+    let dataSkill = document.createElement('li');
+    dataSkill.setAttribute("class","skill");
+    
+    let imgSkills = document.createElement('img');
+    imgSkills.setAttribute("class","imgSkill");
+    imgSkills.src = item.image;
+
+    dataSkill.appendChild(imgSkills);
+
+    listSkillsTech.append(dataSkill);
+
+});
+
+let titleSoftSkills = document.createElement('h3');
+titleSoftSkills.textContent = "Habilidades blandas";
+
+let listSoftSkills = document.createElement('ul');
+listSoftSkills.className = "list-soft-skills";
+
+arrayData.softSkills.forEach( row =>{
+
+    let dataSoftSkill = document.createElement('li');
+    dataSoftSkill.setAttribute("class","Softskill");
+    
+    let imgSoftSkills = document.createElement('img');
+    imgSoftSkills.setAttribute("class","imgSSoftkill");
+    imgSoftSkills.src = row.image;
+
+    let nameSkill = document.createElement('h5');
+    nameSkill.textContent = row.name;
+
+    dataSoftSkill.append(imgSoftSkills, nameSkill);
+
+    listSoftSkills.append(dataSoftSkill);
+});
+
+skillsSection.append(titleSkills, titleSkillsTech, listSkillsTech, titleSoftSkills, listSoftSkills );
+
+
+// proyect section
+
+let proyectSection = document.createElement('section');
+proyectSection.setAttribute('id', 'proyects');
+
+let titleProyects = document.createElement('h1');
+titleProyects.textContent = "Proyectos";
+
+let listProtects = document.createElement('ul');
+listProtects.className = "list-proyects";
+
+arrayData.proyects.forEach(doc =>{
+
+    let dataProyect = document.createElement('li');
+    dataProyect.className ="cardProyect";
+
+    let titleProyect = document.createElement('h3');
+    titleProyect.textContent = doc.name;
+
+    let imgProyect = document.createElement('img');
+    imgProyect.className = "imageProyect";
+    imgProyect.src = doc.image;
+
+    let descriptionProyect = document.createElement('p');
+    descriptionProyect.className = "descriptionProyect";
+    descriptionProyect.textContent = doc.description;
+
+  
+    let urlProyect = document.createElement('a');
+    urlProyect.className = "urlProyect";
+    urlProyect.href = doc.url;
+
+    let iconUrl = document.createElement('img');
+    iconUrl.src = "img/vistas.png";
+
+    urlProyect.append(iconUrl);
+
+    let urlRespository = document.createElement('a');
+    urlRespository.className = "urlRespository";
+    urlRespository.href = doc.repository;
+
+    let iconRespository = document.createElement('img');
+    iconRespository.src = "img/github.png";
+
+    urlRespository.append(iconRespository);
+
+    let divIcons = document.createElement('div');
+    divIcons.className = "contentIcons";
+    
+    let buttonUrl = document.createElement('button');
+    buttonUrl.className = "btn-url";
+    buttonUrl.textContent = "Ver Detalle";
+
+    divIcons.append(urlProyect, urlRespository, buttonUrl);
+    
+    dataProyect.append( titleProyect, imgProyect, descriptionProyect, divIcons);
+    listProtects.append(dataProyect);
+
+
+});
+
+
+proyectSection.append(titleProyects, listProtects);
+
+
+// curse section
+
+let couserSection = document.createElement('section');
+couserSection.setAttribute('id', 'couses');
+
+
+let titleCursos = document.createElement('h1');
+titleCursos.textContent = "Cursos";
+
+let listCurses = document.createElement('ul');
+listCurses.className = "listCurses";
+
+arrayData.couses.forEach(cur =>{
+
+    let datacurses = document.createElement('li');
+    datacurses.setAttribute("class","curse");
+
+    let titlecurse = document.createElement('h2');
+    titlecurse.textContent = cur.name;
+    
+    let academyCurse = document.createElement('h4');
+    academyCurse.textContent = cur.academy;
+    
+    let dateCurse = document.createElement('p');
+    dateCurse.textContent = cur.date;
+    let urlcurse = document.createElement('a');
+    urlcurse.href = cur.url;
+    urlcurse.textContent = "Ver curso";
+    datacurses.append(titlecurse, academyCurse, dateCurse, urlcurse);
+
+    listCurses.append(datacurses);
+    
+})
+
+
+couserSection.append(titleCursos, listCurses);
+
+
+// experience section
+let experienceSection = document.createElement('section');
+experienceSection.setAttribute('id', 'experience');
+
+
+let titleexperience = document.createElement('h1');
+titleexperience.textContent = "Experiencia Profecional";
+
+let listexperience = document.createElement('ul');
+listexperience.className = "listexpe";
+
+arrayData.experience.forEach(ex =>{
+
+    
+    let dataExperi = document.createElement('li');
+    dataExperi.setAttribute("class","exprience");
+
+    let titleExprience = document.createElement('h2');
+    titleExprience.textContent = ex.company;
+
+    let positionExperience = document.createElement('h4');
+    positionExperience.textContent = ex.position
+
+    let dateExperience = document.createElement('h5');
+    dateExperience.textContent = `${ex.startDate} - ${ex.endDate}`;
+
+    let descriptionExperience = document.createElement('ul');
+
+    ex.responsibilities.forEach(listdes =>{
+        let descriptionRespo = document.createElement('li');
+        descriptionRespo.textContent = listdes;
+        descriptionExperience.append(descriptionRespo);
+    })
+
+    
+    dataExperi.append(titleExprience, positionExperience,dateExperience, descriptionExperience);
+
+
+    console.log(ex);
+
+    listexperience.append(dataExperi);
+})
+
+experienceSection.append(titleexperience, listexperience);
+
+container.append(header, sectionAboutme, skillsSection, proyectSection, couserSection, experienceSection);
+
+
+// desplazamientos de enlases
+
+lis1.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    sectionAboutme.scrollIntoView({ behavior: 'smooth' });
+});
+
+lis2.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+lis3.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    proyectSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+lis4.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    couserSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+lis5.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    experienceSection.scrollIntoView({ behavior: 'smooth' });
+});
